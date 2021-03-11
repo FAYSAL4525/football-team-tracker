6a46c682f183b54c39e601ec6187e09fa4fe45cc
FAYSAL4525 <faysalahamed306@gmail.com>
2021-03-11 23:35:55 +0600

    ffootball-team-tracker

--- src/components/AllTeamDetail/AllTeamDetails.js
+++ src/components/AllTeamDetail/AllTeamDetails.js
@@ -7,13 +7,13 @@ import TeamDetails from '../TeamDetails/TeamDetails';
 const AllTeamDetails = () => {
     const {teamid}=useParams();
     const [teamDetail,setTeamDetail]=useState([]);
-    const {idTeam}=teamDetail;
+    // const {idTeam}=teamDetail;
     useEffect(()=>{
         const url=`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamid}`
         fetch(url)
         .then(res=>res.json())
         .then(data=>setTeamDetail(data.teams))
-    },[]);
+    },[teamid]);
     return (
         <div>
           {
